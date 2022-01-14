e62dde3a1bff16b2c7621dbbefa7fe574137c537
Divyansh Rai <51046729+DivyanshRai27@users.noreply.github.com>
2022-01-14 15:46:32 +0530

    create user

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { Sequelize } = require("sequelize");
 const models = require("./models");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   models.User.findAll()
@@ -24,6 +26,24 @@ app.get("/posts", (req, res) => {
       res.sendStatus(200);
     })
     .catch((err) => console.log(err));
+});
+
+app.post("/create-user", async (req, res) => {
+  const user_name = await models.User.create({
+    id: req.body.id,
+    name: req.body.name,
+    email: req.body.email,
+  });
+  res.send("Done");
+});
+
+app.post("/create-post", async (req, res) => {
+  const post_name = await models.Post.create({
+    id: req.body.id,
+    title: req.body.title,
+    // user_id: req.body.user_id,
+  });
+  res.send("Done");
 });
 
 const PORT = process.env.PORT || 3000;
